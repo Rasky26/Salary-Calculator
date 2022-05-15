e94d72c806a31e962ce47c33431b861a49e6b732
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-05-15 07:52:41 -0500

    Set up the first function for button click submission

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,31 @@
 console.log('Heyo')
+
+// --------------------------------
+// Global variables
+let employees
+
+
+// --------------------------------
+// Ensure the document is ready
+$(readyNow)
+
+
+// --------------------------------
+// Main function that holds the handlers that
+// impact the DOM
+function readyNow() {
+
+    // Button click handler - handles the input fields and
+    // updates the DOM with new employees
+    $(document).on('click', '#button-add-employee', submitHandler)
+}
+
+
+// --------------------------------
+// Function that takes in the input values, validates
+// the information, and updates the table and running
+// total of salaries.
+function submitHandler() {
+
+    console.log('Made it to the `submitHandler`')
+}
